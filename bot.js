83b6818342b2f3bfeb8337de319523adad0e1571
Felipe Dae Gomes <84628638+medaegomes@users.noreply.github.com>
2024-07-23 17:45:09 +0000

    update .env variable

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const env = process.env // puxa variáveis env da hospedagem | para puxar do arquivo .env substitua para > const env = require('./.env') <
+const env = BOT_TOKEN // puxa variáveis env da hospedagem | para puxar do arquivo .env substitua para > const env = require('./.env') <
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
